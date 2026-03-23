86e40d323efce52f8059da36073cbab5a0d19f30
Muadil87 <elbahlouli.adil@etu.uae.ac.ma>
2026-03-23 20:25:30 +0100

    Step 2: Created and registered faculty, student, and home_auth apps

--- school/school/settings.py
+++ school/school/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'faculty',
+    'student',
+    'home_auth',
 ]
 
 MIDDLEWARE = [
